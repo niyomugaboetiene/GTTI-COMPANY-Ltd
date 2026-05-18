625d86c52c25df92cb7cd68f4047c2d3f96e8212
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-18 09:34:13 +0200

    Commit of frontend

---
+++ frontend/src/pages/ExportList.jsx
@@ -0,0 +1,74 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function ExportList() {
+
+    const [exportsData, setExportsData] = useState([]);
+
+    const getExports = async () => {
+
+        const res = await axios.get(
+            "http://localhost:5000/exports"
+        );
+
+        setExportsData(res.data);
+    };
+
+    useEffect(() => {
+        getExports();
+    }, []);
+
+    return (
+
+        <div className="p-6">
+
+            <div className="bg-white p-5 rounded shadow">
+
+                <h1 className="text-2xl font-bold mb-4">
+                    Export List
+                </h1>
+
+                <table className="w-full border">
+
+                    <thead className="bg-gray-200">
+
+                        <tr>
+
+                            <th className="border p-2">Food</th>
+                            <th className="border p-2">Quantity</th>
+
+                        </tr>
+
+                    </thead>
+
+                    <tbody>
+
+                        {
+                            exportsData.map((item) => (
+
+                                <tr key={item._id}>
+
+                                    <td className="border p-2">
+                                        {item.foodId?.foodName}
+                                    </td>
+
+                                    <td className="border p-2">
+                                        {item.quantity}
+                                    </td>
+
+                                </tr>
+
+                            ))
+                        }
+
+                    </tbody>
+
+                </table>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default ExportList
