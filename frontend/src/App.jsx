d3c58d972f40d357481ea1e4455db2ea857ed7fa
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-19 11:46:45 +0200

    Commit of frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -42,8 +42,8 @@ function App() {
         </nav>
         </div>
         <div className="flex space-x-3">
-          <button className="bg-blue-900 text-white px-4 h-8 mt-3">Register</button>
-          <button className="bg-blue-900 text-white px-4 h-8 mt-3">Login</button>
+          <button className="bg-blue-900 text-white px-4 h-8 mt-3 hover:bg-blue-800 transition-colors">Register</button>
+          <button className="bg-blue-900 text-white px-4 h-8 mt-3 hover:bg-blue-800 transition-colors">Login</button>
         </div>
      </div>
 
