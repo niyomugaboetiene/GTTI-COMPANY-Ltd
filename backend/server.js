0e5f1703ce2af96568e3f5b899247a1aa6033123
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-19 13:07:45 +0200

    Commit of frontend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'http://localhost:5173',
+    credentials: true
 }));
 app.use(session({
     resave: false,
