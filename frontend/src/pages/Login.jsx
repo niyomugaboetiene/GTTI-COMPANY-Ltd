d24cc8db8cca6b9afb9b4958e54c2aae0739c6c9
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-19 11:55:49 +0200

    Commit of frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
 
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -20,6 +23,7 @@ function Login() {
             // reset fields
             setUserName("");
             setPassword("");
+            navigate('/')
 
         } catch (err) {
             console.error(err);
