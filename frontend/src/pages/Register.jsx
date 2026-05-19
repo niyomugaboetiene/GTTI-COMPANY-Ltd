5df806e8d7fef56c9550ce2d918196d4f4c6bc96
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-19 11:52:01 +0200

    Commit of frontend

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,79 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function Register() {
+
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await axios.post(
+                "http://localhost:5000/manager/login",
+                { userName, password }
+            );
+
+            alert("Import Added");
+
+            // reset fields
+            setUserName("");
+            setPassword("");
+
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    return (
+        <div className="bg-gray-100 min-h-screen flex justify-center items-center">
+
+            <form
+                onSubmit={handleSubmit}
+                className="bg-blue-100 p-6 w-full max-w-md rounded-xl shadow-lg"
+            >
+
+                <h1 className="text-3xl font-bold text-blue-700 mb-6 text-center">
+                    Login Page
+                </h1>
+
+                <div className="mb-4">
+                    <label className="block text-lg text-blue-600 mb-1">
+                        Username
+                    </label>
+
+                    <input
+                        type="text"
+                        onChange={(e) => setUserName(e.target.value)}
+                        className="w-full bg-gray-200 py-3 px-4 rounded-full focus:outline focus:outline-blue-500"
+                    />
+                </div>
+
+                <div className="mb-4">
+                    <label className="block text-lg text-blue-600 mb-1">
+                        Password
+                    </label>
+
+                    <input
+                        type="password"
+                        value={quantity}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="w-full bg-gray-200 py-3 px-4 rounded-full focus:outline focus:outline-blue-500"
+                    />
+                </div>
+
+                <button
+                    type="submit"
+                    className="bg-blue-500 w-full py-3 rounded-full text-white hover:bg-blue-600 transition-colors"
+                >
+                    Login
+                </button>
+
+            </form>
+
+        </div>
+    );
+}
+
+export default Register;
