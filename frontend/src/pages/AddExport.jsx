bcb8164ae25ed255c3be26853245458fc65a72e3
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-19 08:48:53 +0200

    Commit of frontend

--- frontend/src/pages/AddExport.jsx
+++ frontend/src/pages/AddExport.jsx
@@ -3,12 +3,10 @@ import { useEffect, useState } from "react";
 
 function AddExport() {
 
-    const [foods, setFoods] = useState([]);
-
-    const [formData, setFormData] = useState({
-        foodId: "",
-        quantity: ""
-    });
+    const [exportDate, setExportDate] = useState("");
+    const [foodId, setFoodId] = useState("");
+    const [quantity, setQuantity] = useState("");
+    // foodId, exportsData, quantity
 
     const getFoods = async () => {
 
@@ -29,7 +27,7 @@ function AddExport() {
 
         await axios.post(
             "http://localhost:5000/exports/add",
-            formData
+            
         );
 
         alert("Export Added");
@@ -37,63 +35,7 @@ function AddExport() {
 
     return (
 
-        <div className="p-6">
-
-            <form
-                onSubmit={handleSubmit}
-                className="bg-white p-5 rounded shadow"
-            >
-
-                <h1 className="text-2xl font-bold mb-4">
-                    Add Export
-                </h1>
-
-                <select
-                    className="border p-2 w-full mb-3"
-                    onChange={(e) =>
-                        setFormData({
-                            ...formData,
-                            foodId: e.target.value
-                        })
-                    }
-                >
-
-                    <option>Select Food</option>
-
-                    {
-                        foods.map((food) => (
-
-                            <option
-                                key={food._id}
-                                value={food._id}
-                            >
-                                {food.foodName}
-                            </option>
-
-                        ))
-                    }
-
-                </select>
-
-                <input
-                    type="number"
-                    placeholder="Quantity"
-                    className="border p-2 w-full mb-3"
-                    onChange={(e) =>
-                        setFormData({
-                            ...formData,
-                            quantity: e.target.value
-                        })
-                    }
-                />
-
-                <button className="bg-blue-600 text-white px-4 py-2">
-                    Save
-                </button>
-
-            </form>
-
-        </div>
+      
     )
 }
 
