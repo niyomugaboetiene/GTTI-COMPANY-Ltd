1ee5871c45a7c33071d50366316171df7da165c6
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-21 08:57:49 +0200

    Commit of frontend

---
+++ frontend/src/pages/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+
+    return (
+        <div>
+            <div>
+                <div>
+                    <h1>GTTI</h1>
+                    <p>GTII company ltd is company that sells foods </p>
+                </div>
+            </div>
+        </div>
+    )
+}
