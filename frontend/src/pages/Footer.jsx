cff59dfb4f3af5f986a6266e1175b913d770b41e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-21 09:02:49 +0200

    Commit of frontend

--- frontend/src/pages/Footer.jsx
+++ frontend/src/pages/Footer.jsx
@@ -10,7 +10,16 @@ const Footer = () => {
                 </div>
                 <div>
                     <h1>Navs</h1>
-                    <Link></Link>
+                    <Link>Foods</Link>
+                    <Link>Import</Link>
+                    <Link>Export</Link>
+                    <Link>Report</Link>
+                </div>
+
+                <div>
+                    <h1>Contact</h1>
+                    <a href="">niyomugaboetiene53@gmail.com</a>
+                    
                 </div>
             </div>
         </div>
