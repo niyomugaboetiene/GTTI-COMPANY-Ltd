7b63a0e4cdb2d54bd2ab4ad3d3389b64fdbc3ce3
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-21 09:16:28 +0200

    Commit of frontend (footer)

--- frontend/src/pages/Footer.jsx
+++ frontend/src/pages/Footer.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
         <div className="bg-blue-600 fixed bottom-0 left-0 right-0 p-2 h-fit text-white text-wrap">
             <div className="flex justify-between">
                 <div className="text-wrap">
-                    <h1>GTTI</h1>
-                    <p>GTII company ltd is company that sells foods </p>
+                    <h1 className="ms-5 text-3xl font-bold ">GTTI</h1>
+                    <p className="ms-5">GTII company ltd is company that sells foods </p>
                 </div>
                 <div className="grid space-y-3">
-                    <h1>Navs</h1>
+                    <h1 className="text-xl font-bold">Navs</h1>
                     <Link>Foods</Link>
                     <Link>Import</Link>
                     <Link>Export</Link>
